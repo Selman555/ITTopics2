38165faff048216e22f04bd5751ae9476809cc54
Anke Appeltans <anke.appeltans@student.phl.be>
2013-10-07 21:58:35 +0200

    verbetering loginform2

--- Topics2Project/codeigniter/application/views/login.php
+++ Topics2Project/codeigniter/application/views/login.php
@@ -10,17 +10,16 @@
 		<?php include 'templates/header.php'; ?>
 
 		<div id="content">
-		  <form id="login">
+		  <form method="post" action="<?php echo base_url(); ?>index.php/VerifyLogin" id="login" >
                       <fieldset id="geheel">
                       <fieldset id="inputsAndTekst">
                         <h1>Log in</h1>
-                      
                             <p>Geef hier uw gebruikersnaam en paswoord in </p>
                             <input id="username" type="text" placeholder="Gebruikersnaam" class="tekstfield" autofocus required>
                             <input id="password" type="password" placeholder="Paswoord" class="tekstfield" required>
                         </fieldset>
                       <fieldset id="btn">
-                          <input type="submit" id="submit" class="submit" value="Log in">
+                          <input type="submit" id="SubmitButton" class="submit" value="Log in">
                             <input type="button" id="passwordRecovery" class="submit" value="Paswoord vergeten?">
                       </fieldset>
                       </fieldset>
