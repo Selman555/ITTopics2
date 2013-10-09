14207dcf36b0b8bba75fb47a2d535d1ebcc18fa0
StevenVerheyen <destination.verheyen@gmail.com>
2013-10-09 22:19:04 +0200

    Spel functioneel

--- Topics2Project/www (Nieuw met php)/game/game.js
+++ Topics2Project/www (Nieuw met php)/game/game.js
@@ -10,8 +10,8 @@ var soundBackground; //Geluid in de achtergrond
 var canvas; //Canvas -> worden componenten op getoond
 const canvasWidth = 900; //Breedte canvas
 const canvasHeight = 512; //Hoogte canvas
-const playerWidth = 28; //Breedte speler
-const playerHeight = 28; //Hoogste speler
+const playerWidth = 32; //Breedte speler
+const playerHeight = 32; //Hoogste speler
 var backgroundVelocity; //Snelheid beeld dat naar links gaat
 var backgroundImg1; //Achtergrond 1
 var backgroundImg2; //Achtergrond 2
@@ -28,18 +28,12 @@ var clousPosY2; //Positie y wolk 2
 var playerImg; //Afbeelding speler
 var playerPosX; //Positie x-as speler
 var playerPosY; //Positie y-as speler
+var playerAllowJump; //Speler heeft toestemming om te springen
 var jumpVelocity; //hoogte van het springen
 var jumpState; //Status van de speler (false != springen - true = springen)
 var goDown; //Speler is omlaag aan het gaan na het springen
 var gravity; //Hoe snel een speler omlaag gaat
 var meters; //Afstand dat de speler gelopen heeft
-var posRectY; //Startpositie y-as rectangle
-var posRectX; //Startpositie x-as rectangle
-var rectXVelocity; //Snelheid dat de rectangle beweegt
-var rectWidth; //Breedte van de rectangle
-var rectHeight; //Hoogte van de rectangle
-var rectColor; //Kleur van de rectangle
-var statusRect; //false = naar links - true = naar rechts
 var userClickSpeed; //Snelheid dat een gebruiker kan kliken
 
 var platformImg;
@@ -61,7 +55,6 @@ var posStartbutton = [];
 var playingSong;
 
 var allowKeyEvent;
-
 var listPlatformPos;
 
 var Debugger = function() { };
@@ -90,14 +83,6 @@ function init() {
 
 	context2D = canvas.getContext("2d");
 	context2D.font = fontType;
-
-	rectWidth = 650;
-	rectHeight = 20;
-	rectColor = "black";
-	posRectY = 150;
-	posRectX = 10;
-	rectXVelocity = 5;
-	statusRect = true;
 
 	cloudImg1 = new Image();
 	cloudImg1.src = "img/clouds.png";
@@ -135,13 +120,14 @@ function init() {
 	playerImg.width = playerWidth;
 	playerImg.height = playerHeight;
 	playerPosX = 200;
-	playerPosY = 100 - playerImg.height; //Beginnen OP de rectangle
+	playerPosY = canvasHeight - 50;
 
 	listPlatformPos = new Array(10); //10 platformen maken
 	platformImg = new Image();
 	platformImg.src = "img/platform.png";
-	platformImg.width = 32;
-	platformImg.height = 32;
+	platformImg.width = 28;
+	platformImg.height = 28;
+	platformPosY = canvasHeight - 45;
 	makePlatforms();
 
 	jumpHeight = 50;
@@ -163,47 +149,28 @@ function init() {
 }
 
 function makePlatforms() {
-
-	for(var i=0; i<10; i++) { //10 posities in de array maken met elk een array van 2 posities
-			listPlatformPos[i] = new Array(2);
-	}
-
-	var posYVorigPlatf = 400;
-	for(var i=0; i<10; i++) { //10 platformen maken
-		platformPosX = i*100;
-
-		//Platformhoogte randrom genereren
-		if(posYVorigPlatf >= 300) {
-			platformPosY = ((Math.random()*4)+3) * 100; //Tussen 3 en 4
-		}else if(posYVorigPlatf >= 200 && posYVorigPlatf < 300) {
-			platformPosY = ((Math.random()*3)+2) * 100; //Tussen 2 en 3
-		}else if(posYVorigPlatf >= 100 && posYVorigPlatf < 200) {
-			platformPosY = ((Math.random()*2)+1) * 100; //Tussen 1 en 2
-		}else if(posYVorigPlatf < 100) {
-			platformPosY = ((Math.random()*1)+1) * 100; //Tussen 1 en 1
-		}
-
-		posYVorigPlatf = platformPosY;
-
-		listPlatformPos[i][0] = platformPosX;
-		listPlatformPos[i][1] = platformPosY;
+	var counter=  0;
+	for(var i=2; i<12; i++) { //10 platformen maken
+		//Onmogelijk om te spelen na 205 meter!!!
+		listPlatformPos[i] = i*200;
+		counter++;
 	}
 
 	Debugger.log("Platformen aangemaakt");
 }
 
 function playBgSound() {
-	//if(!gameover && !dead) {
-	//	if(!playingSong) {
+	if(!gameover && !dead) {
+		if(!playingSong) {
 	//		soundBackground.play();
-	//		playingSong = true;
-	//	}
-	//}
+			playingSong = true;
+		}
+	}
 }
 
 function drawStartscreen() {
 	if(startscreen) {
-		//Canvas leegmaken om weer op te vullen met ofbeeldingen op een ander locatie
+		//Canvas leegmaken om weer op te vullen met afbeeldingen op een ander locatie
 		context2D.clearRect(0, 0, canvasWidth, canvasHeight);
 
 		//Achtergrond tekenen
@@ -239,9 +206,9 @@ function scrollCloudsStartscreen(vel) {
 
 function scrollPlatforms(vel) {
 	for(var i=0; i<listPlatformPos.length-1; i++) {
-		listPlatformPos[i][0] -= vel;
-		if(listPlatformPos[i][0]+platformImg.width < 0) {
-			listPlatformPos[i][0] = canvasWidth;
+		listPlatformPos[i] -= vel;
+		if(listPlatformPos[i]+platformImg.width < 0) {
+			listPlatformPos[i] = canvasWidth;
 		}
 	}
 }
@@ -257,55 +224,22 @@ function scrollBackground(vel) {
 	}
 }
 
-function gravityPlayer(vel) {
-	if(!jumpState && !goDown) { //Zorgen voor springen
-		if(playerPosY+playerHeight < listPlatformPos[0][1]) {
-			playerPosY+=3;
-			//Voorlopig elk platform nakijken
-			if((playerPosX >= listPlatformPos[0][0] && playerPosX+playerImg.width <= listPlatformPos[0][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[1][0] && playerPosX+playerImg.width <= listPlatformPos[1][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[2][0] && playerPosX+playerImg.width <= listPlatformPos[2][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[3][0] && playerPosX+playerImg.width <= listPlatformPos[3][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[4][0] && playerPosX+playerImg.width <= listPlatformPos[4][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[5][0] && playerPosX+playerImg.width <= listPlatformPos[5][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[6][0] && playerPosX+playerImg.width <= listPlatformPos[6][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[7][0] && playerPosX+playerImg.width <= listPlatformPos[7][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[8][0] && playerPosX+playerImg.width <= listPlatformPos[8][0]+platformImg.width) && 
-				(playerPosX >= listPlatformPos[9][0] && playerPosX+playerImg.width <= listPlatformPos[9][0]+platformImg.width)) 
-			{
-				playerPosY=+3;
+function collisionPlayer() {
+	for(var i=0; i<10; i++) { //Elke platform controleren
+	if((playerPosX >= listPlatformPos[i] && playerPosX <= listPlatformPos[i]) && //Positie x-as controleren
+	 (playerPosY+playerImg.height >= platformPosY)) { //Positie y-as controleren
+			//Collision
 
-				//Kijken als de speler de grond raakt
-				if(playerPosY + playerHeight > canvasHeight) {
-					dead = true;
-					gameover = true;
-					playing = false;
-				}
-			}
+			soundDead.play();
+			dead = true;
+			highscore = true;
+			playing = false;
+
+			clearInterval(intervalPlaying);
+			Debugger.log("Speler raakt platform");
+
+			break;
 		}
-
-
-		//Onderstaande zit een fout
-		//for(var i=0; i<listPlatformPos.length-1; i++) { //Voor elk platform nakijken
-		//	if(!(playerPosX >= listPlatformPos[i][0] && playerPosX+playerImg.width <= listPlatformPos[i][0]+platformImg.width)) { //Als de speler op het platform staat
-		//		if(!(playerPosY-playerHeight > listPlatformPos[i][1])) {
-		//			playerPosY += vel;
-		//			Debugger.log("Speler valt");
-		//		} else {
-		//			dead = true;
-		//			gameover = true;
-		//			playing = false;
-
-		//			Debugger.log("Speler is dood");
-		//		}
-		//	} else {
-		//		dead = true;
-		//		gameover = true;
-		//		playing = false;
-
-		//		Debugger.log("Speler is dood");
-		//	}
-		//}
 	}
 }
 
@@ -315,14 +249,19 @@ function draw() { //Deze wordt bij het spelen ALTIJD opgeroepen (zie het als een
 	context2D.drawImage(backgroundImg1, backgroundPosX1, backgroundPosY1, backgroundImg1.width, backgroundImg1.height);
 	context2D.drawImage(backgroundImg2, backgroundPosX2, backgroundPosY2, backgroundImg2.width, backgroundImg2.height);
 
+	//Wolken tekenen
+	context2D.drawImage(cloudImg1, cloudPosX1, cloudPosY1, cloudImg1.width, cloudImg1.height);
+	context2D.drawImage(cloudImg2, cloudPosX2, cloudPosY2, cloudImg2.width, cloudImg2.height);
+
 	context2D.fillText("Meters: " + meters, 30, 30);
 	
 	drawPlatforms();
-	gravityPlayer(1);
+	collisionPlayer();
 
 	if(!gameover) {
-		scrollBackground(3);
-		//playBgSound();
+		scrollBackground(5);
+		scrollCloudsStartscreen(7); //Wolken laten scrollen
+		playBgSound();
 		meters++;
 
 		drawPlayer();
@@ -349,29 +288,28 @@ function drawStartbutton() {
 	context2D.fillRect(buttonPosX, buttonPosY, buttonWidth, buttonHeight);
 
 	context2D.fillStyle = "white";
-	context2D.fillText("Start", canvasWidth/2 - buttonWidth/2+25, buttonPosY + buttonHeight/2); //Text, x, y
-
+	context2D.fillText("Start", canvasWidth/2 - buttonWidth/2+25, buttonPosY + buttonHeight/2+8); //Text, x, y
 
 	Debugger.log("Startknop gemaakt");
 }
 
 function drawPlatforms() {
 	//context2D.clearRect(0, 0, canvasWidth, canvasHeight);
-	scrollPlatforms(5);
+	scrollPlatforms(10);
 
 	for(var i=0; i<listPlatformPos.length-1; i++) {
-		context2D.drawImage(platformImg, listPlatformPos[i][0], listPlatformPos[i][1], platformImg.width, platformImg.height);
+		context2D.drawImage(platformImg, listPlatformPos[i], platformPosY, platformImg.width, platformImg.height);
 	}
 	Debugger.log("Platformen getekend");
 }
 
 function jumping() {
-	if(!gameover && !startscreen) {
+	if(!gameover && !startscreen && playing) {
 		if(!jumpState) {
 			jumpState = true;
 			allowKeyEvent = false;
 			intervalJumping = setInterval (function() {
-				playerPosY--;
+				playerPosY-=3;
 				drawPlayer();
 
 				Debugger.log("Aan het springen");
@@ -381,23 +319,26 @@ function jumping() {
 			//jumpState = true;
 			soundJump.currentTime = 0;
 			soundJump.play();
-			setTimeout(goingDown, 500); //Bepaalt hoe lang de speler kan springen (500ms)
+			setTimeout(goingDown, 250); //Bepaalt hoe lang de speler kan springen (500ms)
 		}
 	}
 }
 
 function goingDown() {
-	if(!gameover && !startscreen) {
+	if(!gameover || !startscreen && playing) {
 		if(jumpState) {
 			goDown = true;
 			clearInterval(intervalJumping);
 
-			intervalGoingDown = setInterval(function() {
-				playerPosY++;
-				drawPlayer();
+				//Bij springen raak je de grond niet meer
+				intervalGoingDown = setInterval(function() {
+						playerPosY+=3;
+						drawPlayer();
+					Debugger.log("Aan het neerkomen");
+				});
 
-				Debugger.log("Aan het neerkomen");
-			});
+				playerPosY+=2;
+		
 
 			jumpState = false;
 
@@ -405,7 +346,7 @@ function goingDown() {
 				goDown = false;
 				clearInterval(intervalGoingDown);
 				allowKeyEvent = true;
-			}, 500);
+			}, 250);
 		}
 	}
 }
