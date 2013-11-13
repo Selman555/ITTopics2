32cc73443618d98be9e8dd44d0655be01b8c0187
selman555 <freestylerstyler760@hotmail.com>
2013-11-13 15:03:05 +0100

    toegevoegd: tasks

---
+++ Topics2Project/codeigniter/application/views/tasks.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="NL-be">
+    <head>
+        <link rel="stylesheet" href="<?php echo base_url('styles/main.css'); ?>" type="text/css" media="screen"/>
+        <link rel="stylesheet" href="<?php echo base_url('styles/contact.css'); ?>" type="text/css" media="screen"/>
+        <title>Taken</title>
+    </head>
+    <body>
+        <?php include('templates/header.php'); ?>
+        <section>
+            <h1>Behaalde Punten</h1>
+            <table style="width: 90%; text-align: left; margin:auto; padding:1%;">
+                <tr>
+                    <th>Naam</th>
+                    <th>Omschrijving</th>
+                    <th>Behaald</th>
+                    <th>Maximum</th>
+                </tr>
+                
+                <?php foreach($tasks as $row) {?>
+                    <tr>
+                        <td><?php echo $row['naam']; ?></td>
+                        <td><?php echo $row['omschrijving']; ?></td>
+                        <td><?php echo $row['puntbehaald']; ?></td>
+                        <td><?php echo $row['puntmogelijk']; ?></td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </section>
+    <?php include('templates/footer.php'); ?>
+    </body>
+</html>
